b294686af5a97bd5f5ccccf84373555ce9ec6287
playboibeans <124590317+playboibeans@users.noreply.github.com>
2023-04-03 00:03:02 +0800

    add coin market data on hover

--- scripts.js
+++ scripts.js
@@ -113,7 +113,7 @@ function searchCoin() {
 }
 
 
-function fetchCryptoPrice(coin, iconID, statusBarClass, statusBarFillClass, priceID, percentID, athID, coinName) {
+function fetchCryptoPrice(coin, iconID, statusBarClass, statusBarFillClass, priceID, percentID, athID, coinName, tooltip) {
     fetch(`https://api.coingecko.com/api/v3/coins/${coin}`)
         .then(response => response.json())
         .then((data) => {
@@ -131,6 +131,12 @@ function fetchCryptoPrice(coin, iconID, statusBarClass, statusBarFillClass, pric
             document.getElementById(percentID).innerHTML = `${(athPercentComplete * 100).toFixed(2)}%`;
             document.getElementById(athID).innerHTML = `ATH:$${crypto_ATH.toLocaleString()} `;
             athStatusBarFill.style.width = athPercentString;
+            const marketcap = data.market_data.market_cap.usd;
+            const change24h = data.market_data.price_change_percentage_24h
+            const change7d = data.market_data.price_change_percentage_7d
+            const change30d = data.market_data.price_change_percentage_30d
+            document.getElementById(tooltip).textContent = `Market Cap: $${marketcap.toLocaleString()}\n24h price change: ${change24h.toFixed(2)}%\n7d price change: ${change7d.toFixed(2)}%\n30d price change: ${change30d.toFixed(2)}%`;
+
         })
         .catch(error => {
             console.log(error);
@@ -139,86 +145,84 @@ function fetchCryptoPrice(coin, iconID, statusBarClass, statusBarFillClass, pric
 
 
 function btcPrice() {
-    fetchCryptoPrice('bitcoin', 'btc-Icon', 'btc-status-bar', 'btc-status-bar-fill', 'btc-price', 'btc-percent', 'btc-Ath', 'btc-Name');
+    fetchCryptoPrice('bitcoin', 'btc-Icon', 'btc-status-bar', 'btc-status-bar-fill', 'btc-price', 'btc-percent', 'btc-Ath', 'btc-Name', 'btc-tooltip');
 }
 
 function ethPrice() {
-    fetchCryptoPrice('ethereum', 'eth-Icon', 'eth-status-bar', 'eth-status-bar-fill', 'eth-price', 'eth-percent', 'eth-Ath', 'eth-Name');
+    fetchCryptoPrice('ethereum', 'eth-Icon', 'eth-status-bar', 'eth-status-bar-fill', 'eth-price', 'eth-percent', 'eth-Ath', 'eth-Name', 'eth-tooltip');
 }
 
 function bnbPrice() {
-    fetchCryptoPrice('binancecoin', 'bnb-Icon', 'bnb-status-bar', 'bnb-status-bar-fill', 'bnb-price', 'bnb-percent', 'bnb-Ath', 'bnb-Name');
+    fetchCryptoPrice('binancecoin', 'bnb-Icon', 'bnb-status-bar', 'bnb-status-bar-fill', 'bnb-price', 'bnb-percent', 'bnb-Ath', 'bnb-Name', 'bnb-tooltip');
 }
 
 function xrpPrice() {
-    fetchCryptoPrice('ripple', 'xrp-Icon', 'xrp-status-bar', 'xrp-status-bar-fill', 'xrp-price', 'xrp-percent', 'xrp-Ath', 'xrp-Name');
+    fetchCryptoPrice('ripple', 'xrp-Icon', 'xrp-status-bar', 'xrp-status-bar-fill', 'xrp-price', 'xrp-percent', 'xrp-Ath', 'xrp-Name', 'xrp-tooltip');
 }
 
 function dogePrice() {
-    fetchCryptoPrice('dogecoin', 'doge-Icon', 'doge-status-bar', 'doge-status-bar-fill', 'doge-price', 'doge-percent', 'doge-Ath', 'doge-Name');
+    fetchCryptoPrice('dogecoin', 'doge-Icon', 'doge-status-bar', 'doge-status-bar-fill', 'doge-price', 'doge-percent', 'doge-Ath', 'doge-Name', 'doge-tooltip');
 }
 
 function adaPrice() {
-    fetchCryptoPrice('cardano', 'ada-Icon', 'ada-status-bar', 'ada-status-bar-fill', 'ada-price', 'ada-percent', 'ada-Ath', 'ada-Name');
+    fetchCryptoPrice('cardano', 'ada-Icon', 'ada-status-bar', 'ada-status-bar-fill', 'ada-price', 'ada-percent', 'ada-Ath', 'ada-Name', 'ada-tooltip');
 }
 
 function solPrice() {
-    fetchCryptoPrice('solana', 'sol-Icon', 'sol-status-bar', 'sol-status-bar-fill', 'sol-price', 'sol-percent', 'sol-Ath', 'sol-Name');
+    fetchCryptoPrice('solana', 'sol-Icon', 'sol-status-bar', 'sol-status-bar-fill', 'sol-price', 'sol-percent', 'sol-Ath', 'sol-Name', 'sol-tooltip');
 }
 
 function shibPrice() {
-    fetchCryptoPrice('shiba-inu', 'shib-Icon', 'shib-status-bar', 'shib-status-bar-fill', 'shib-price', 'shib-percent', 'shib-Ath', 'shib-Name');
+    fetchCryptoPrice('shiba-inu', 'shib-Icon', 'shib-status-bar', 'shib-status-bar-fill', 'shib-price', 'shib-percent', 'shib-Ath', 'shib-Name', 'shib-tooltip');
 }
 
 function maticPrice() {
-    fetchCryptoPrice('matic-network', 'matic-Icon', 'matic-status-bar', 'matic-status-bar-fill', 'matic-price', 'matic-percent', 'matic-Ath', 'matic-Name');
+    fetchCryptoPrice('matic-network', 'matic-Icon', 'matic-status-bar', 'matic-status-bar-fill', 'matic-price', 'matic-percent', 'matic-Ath', 'matic-Name', 'matic-tooltip');
 }
 
 function ltcPrice() {
-    fetchCryptoPrice('litecoin', 'ltc-Icon', 'ltc-status-bar', 'ltc-status-bar-fill', 'ltc-price', 'ltc-percent', 'ltc-Ath', 'ltc-Name');
+    fetchCryptoPrice('litecoin', 'ltc-Icon', 'ltc-status-bar', 'ltc-status-bar-fill', 'ltc-price', 'ltc-percent', 'ltc-Ath', 'ltc-Name', 'ltc-tooltip');
 }
 
 function dotPrice() {
-    fetchCryptoPrice('polkadot', 'dot-Icon', 'dot-status-bar', 'dot-status-bar-fill', 'dot-price', 'dot-percent', 'dot-Ath', 'dot-Name');
+    fetchCryptoPrice('polkadot', 'dot-Icon', 'dot-status-bar', 'dot-status-bar-fill', 'dot-price', 'dot-percent', 'dot-Ath', 'dot-Name', 'dot-tooltip');
 }
 
 function avaxPrice() {
-    fetchCryptoPrice('avalanche-2', 'avax-Icon', 'avax-status-bar', 'avax-status-bar-fill', 'avax-price', 'avax-percent', 'avax-Ath', 'avax-Name');
+    fetchCryptoPrice('avalanche-2', 'avax-Icon', 'avax-status-bar', 'avax-status-bar-fill', 'avax-price', 'avax-percent', 'avax-Ath', 'avax-Name', 'avax-tooltip');
 }
 
 function arbPrice() {
-    fetchCryptoPrice('arbitrum', 'arb-Icon', 'arb-status-bar', 'arb-status-bar-fill', 'arb-price', 'arb-percent', 'arb-Ath', 'arb-Name');
+    fetchCryptoPrice('arbitrum', 'arb-Icon', 'arb-status-bar', 'arb-status-bar-fill', 'arb-price', 'arb-percent', 'arb-Ath', 'arb-Name', 'arb-tooltip');
 }
 
 function opPrice() {
-    fetchCryptoPrice('optimism', 'op-Icon', 'op-status-bar', 'op-status-bar-fill', 'op-price', 'op-percent', 'op-Ath', 'op-Name');
+    fetchCryptoPrice('optimism', 'op-Icon', 'op-status-bar', 'op-status-bar-fill', 'op-price', 'op-percent', 'op-Ath', 'op-Name', 'op-tooltip');
 }
 
 function astrPrice() {
-    fetchCryptoPrice('astar', 'astr-Icon', 'astr-status-bar', 'astr-status-bar-fill', 'astr-price', 'astr-percent', 'astr-Ath', 'astr-Name');
+    fetchCryptoPrice('astar', 'astr-Icon', 'astr-status-bar', 'astr-status-bar-fill', 'astr-price', 'astr-percent', 'astr-Ath', 'astr-Name', 'astr-tooltip');
 }
 
 function trxPrice() {
-    fetchCryptoPrice('tron', 'trx-Icon', 'trx-status-bar', 'trx-status-bar-fill', 'trx-price', 'trx-percent', 'trx-Ath', 'trx-Name');
+    fetchCryptoPrice('tron', 'trx-Icon', 'trx-status-bar', 'trx-status-bar-fill', 'trx-price', 'trx-percent', 'trx-Ath', 'trx-Name', 'trx-tooltip');
 }
 
 function uniPrice() {
-    fetchCryptoPrice('uniswap', 'uni-Icon', 'uni-status-bar', 'uni-status-bar-fill', 'uni-price', 'uni-percent', 'uni-Ath', 'uni-Name');
+    fetchCryptoPrice('uniswap', 'uni-Icon', 'uni-status-bar', 'uni-status-bar-fill', 'uni-price', 'uni-percent', 'uni-Ath', 'uni-Name', 'uni-tooltip');
 }
 function linkPrice() {
-    fetchCryptoPrice('chainlink', 'link-Icon', 'link-status-bar', 'link-status-bar-fill', 'link-price', 'link-percent', 'link-Ath', 'link-Name');
+    fetchCryptoPrice('chainlink', 'link-Icon', 'link-status-bar', 'link-status-bar-fill', 'link-price', 'link-percent', 'link-Ath', 'link-Name', 'link-tooltip');
 }
 
 function atomPrice() {
-    fetchCryptoPrice('cosmos', 'atom-Icon', 'atom-status-bar', 'atom-status-bar-fill', 'atom-price', 'atom-percent', 'atom-Ath', 'atom-Name');
+    fetchCryptoPrice('cosmos', 'atom-Icon', 'atom-status-bar', 'atom-status-bar-fill', 'atom-price', 'atom-percent', 'atom-Ath', 'atom-Name', 'atom-tooltip');
 }
 
 function etcPrice() {
-    fetchCryptoPrice('ethereum-classic', 'etc-Icon', 'etc-status-bar', 'etc-status-bar-fill', 'etc-price', 'etc-percent', 'etc-Ath', 'etc-Name');
+    fetchCryptoPrice('ethereum-classic', 'etc-Icon', 'etc-status-bar', 'etc-status-bar-fill', 'etc-price', 'etc-percent', 'etc-Ath', 'etc-Name', 'etc-tooltip');
 }
-function etcPrice() {
-    fetchCryptoPrice('ethereum-classic', 'etc-Icon', 'etc-status-bar', 'etc-status-bar-fill', 'etc-price', 'etc-percent', 'etc-Ath', 'etc-Name');
-}
+
 function xmrPrice() {
-    fetchCryptoPrice('monero', 'xmr-Icon', 'xmr-status-bar', 'xmr-status-bar-fill', 'xmr-price', 'xmr-percent', 'xmr-Ath', 'xmr-Name');
+    fetchCryptoPrice('monero', 'xmr-Icon', 'xmr-status-bar', 'xmr-status-bar-fill', 'xmr-price', 'xmr-percent', 'xmr-Ath', 'xmr-Name', 'xmr-tooltip');
 }
